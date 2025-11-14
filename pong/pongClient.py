f6d6fa3204e08a9fa943d7b760981666478c86d1
GarrettMcNeill2 <garrettmcneill04@gmail.com>
2025-11-14 13:31:11 -0500

    no message

--- pong/pongClient.py
+++ pong/pongClient.py
@@ -10,7 +10,6 @@ import pygame
 import tkinter as tk
 import sys
 import socket
-
 from assets.code.helperCode import *
 
 # This is the main game loop.  For the most part, you will not need to modify this.  The sections
